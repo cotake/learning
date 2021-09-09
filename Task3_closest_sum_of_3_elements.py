f55a92ef5d2f85986bab78b937154055d710f3b4
cotake <90404033+cotake@users.noreply.github.com>
2021-09-09 18:37:02 +0300

    Add files via upload
    test

---
+++ Task3_closest_sum_of_3_elements.py
@@ -0,0 +1,22 @@
+def closest_sum(nums, target):
+    current = {}
+    for i in range(0, len(nums) - 2):
+        for j in range(i + 1, len(nums) - 1):
+            if j == len(nums):
+                break
+            for k in range(j + 1, len(nums)):
+                if k == len(nums):
+                    break
+                else:
+                    current[f'{nums[i]} {nums[j]} {nums[k]}'] = sum([nums[i], nums[j], nums[k]])
+    lowest_delta = abs(target - (nums[0] + nums[1] + nums[2]))
+    index_target = f'{nums[0]} {nums[1]} {nums[2]}'
+    for key in current.keys():
+        if abs(target - current[key]) < lowest_delta:
+            lowest_delta = abs(target - current[key])
+            index_target = key
+    return current[index_target]
+
+
+print(closest_sum([1, -5, 6, 4], 5))
+
